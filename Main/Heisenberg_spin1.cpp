6547b6366c6064cce8c34aeaf17d894e789ca356
ZLinTsai <zlintsai@gmail.com>
2019-08-23 21:36:38 +0800

    Update

--- Main/Heisenberg_spin1.cpp
+++ Main/Heisenberg_spin1.cpp
@@ -98,7 +98,7 @@ void tSDRG_XXZ(int L, int chi, int Pdis, double Jdis, string BC, double S, doubl
     vector<double> En = J_list;                  // J_list will earse to one, and return ground energy.
     bool info = 1;                               // True; if tSDRG can not find non-zero gap, info return 0, and stop this random seed.
     bool save_RG_info = 0;                       // save gaps at RG stage 
-    tSDRG_PBC(MPO_chain, En, w_up, w_loc, chi, dis, Pdis, Jseed, save_RG_info, info);
+    tSDRG(MPO_chain, En, w_up, w_loc, chi, dis, Pdis, Jseed, save_RG_info, info);
 
     /// check info if can not RG_J
     if (info == 0)
